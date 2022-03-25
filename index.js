a3b3bc089c133e5829066d3d5849817fa308467b
PabloFM <pafmon@gmail.com>
2022-03-25 12:05:20 +0100

    updated

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 app.get("/api", (req,res)=>{
-    //res.send({ result: true });
-    res.sendStatus(400);
+    res.send({ result: true });
 });
 
 app.listen(port, () => {
