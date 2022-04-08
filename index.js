916f19ebd9c7a2851c53ec51ef5571a3931ef1b3
PabloFM <pafmon@gmail.com>
2022-04-08 09:57:00 +0200

    finish l08

--- index.js
+++ index.js
@@ -3,8 +3,19 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.get("/api", (req,res)=>{
-    res.send({ result: true });
+app.use("/",express.static("./public"));
+
+app.get("/api/v1/contacts", (req,res)=>{
+    res.send([
+        {
+            name:"pedro",
+            phone: 12345
+        },
+        {
+            name:"pablo",
+            phone: 12345           
+        }
+    ]);
 });
 
 app.listen(port, () => {
