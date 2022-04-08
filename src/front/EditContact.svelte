f7fa5ba9ddcc7e8f7caa797389d64516e4e7d81e
PabloFM <pafmon@gmail.com>
2022-04-08 12:31:25 +0200

    updated code

--- src/front/EditContact.svelte
+++ src/front/EditContact.svelte
@@ -1,13 +1,69 @@
 <script>
-
+    import { onMount } from 'svelte';
     export let params = {};
     import { pop } from "svelte-spa-router";
-    import { Button } from "sveltestrap";
- 
+    import { Table,Button } from "sveltestrap";
+    
+    onMount(getContact);
 
+    let contact = {};
+
+    let updatedName;
+    let updatedPhone;
+    let updatedEmail;
+    
+    
+
+    async function getContact(){
+        console.log("Fetching contacts....");
+        const res = await fetch("/api/v1/contacts/"+params.contactName); 
+        if(res.ok){
+            const data = await res.json();
+            contact = data;
+            updatedName = contact.name;
+            updatedEmail = contact.email;
+            updatedPhone = contact.phone;
+            
+            console.log("Received contact.");
+        }
+    }
+
+    async function editContact(){
+    }
 </script>
 
 <main>
-    This is the view of Contact {params.contactName};
+    <h1>Edit contact "{params.contactName}" </h1>
+    {#await contact}
+    loading
+        {:then contact}
+        
+    
+        <Table bordered>
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Phone</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td><input bind:value="{updatedName}"></td>
+                    <td><input bind:value="{updatedEmail}"></td>
+                    <td><input bind:value="{updatedPhone}"></td>
+                    <td><Button 
+                            outline 
+                            color="primary" 
+                            on:click="{editContact}">
+                        Edit
+                        </Button>
+                    </td>
+                </tr>
+            </tbody>
+        </Table>
+    {/await}
     <Button outline color="secondary" on:click="{pop}">Back</Button>
-</main>
+
+    </main>
